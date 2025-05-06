45fd710382b127725fb57a800ed8bced3c7d183d
Pamela Pereyra <ppereyra@Pamelas-Air.fibertel.com.ar>
2025-05-06 05:52:39 -0300

    HomeworkKotlin1-first-part

--- app/src/main/java/com/henryandroid/kotlin/tareas/kotlin2/Kotlin2.kt
+++ app/src/main/java/com/henryandroid/kotlin/tareas/kotlin2/Kotlin2.kt
@@ -5,37 +5,56 @@ class Kotlin2 {
     // 1. Declaración y uso de funciones básicas
     fun greet(name: String) {
         // TODO: Imprimir "Hola, {name}!" usando println
+        println("Hola, $name!")
     }
 
     fun add(a: Int, b: Int): Int {
         // TODO: Retornar la suma de a y b
-        return 0
+        return a + b
     }
 
     // 2. Funciones inline y de una sola expresión
     inline fun multiply(a: Int, b: Int) = a * b
     
-    fun square(n: Int) = n * n // TODO: Convertir esta función a una expresión de una sola línea
+    //fun square(n: Int) = n * n // TODO: Convertir esta función a una expresión de una sola línea
+
+    val square: (Int) -> Int = { n -> n * n }
+    val square2: (Int) -> Int = { it * it }
     
     // 3. Introducción a lambdas y funciones de orden superior
     fun lambdaExample() {
         val numbers = listOf(1, 2, 3, 4, 5)
+
         // TODO: Usar map para duplicar los valores de numbers y almacenarlo en una nueva lista
+        val doubledNumbers = numbers.map { it * 2 }
+        println("Números duplicados: $doubledNumbers")
+
         // TODO: Usar filter para obtener solo los números pares
+        val pairNumbers = numbers.filter { it % 2 == 0 }
+        println("Números pares: $pairNumbers")
     }
 
     // 4. Clases y objetos
     class Person(private val name: String, private var age: Int) {
         // TODO: Agregar un método que imprima "Soy {name} y tengo {age} años"
+
+        fun presentation() {
+            println("Soy $name y tengo $age años")
+        }
     }
 
     // 5. Constructores y datos encapsulados
     class Car(val brand: String, val model: String, val year: Int) {
         init {
             // TODO: Imprimir "Auto creado: {brand} {model} del año {year}"
+            initialize()
+        }
+
+       private fun initialize() {
+            println("Auto creado: $brand $model del año $year")
         }
     }
-    
+
     fun main() {
         // Llamar a las funciones de prueba
         greet("Gaston")
@@ -47,7 +66,13 @@ class Kotlin2 {
         // Creación de objetos y uso de clases
         val person = Person("Juan", 30)
         // TODO: Llamar al método que imprime la información de la persona
-        
+        person.presentation()
+
         val car = Car("Toyota", "Corolla", 2022)
     }
+}
+
+fun main(){
+    val ejercicios = Kotlin2()
+    ejercicios.main()
 }
